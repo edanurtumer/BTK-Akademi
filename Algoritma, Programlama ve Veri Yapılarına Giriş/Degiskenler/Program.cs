09a50e2da1a33bf00a0a1d00580a99dba7902686
Edanur Tümer <62879275+edanurtumer@users.noreply.github.com>
2022-08-25 22:02:46 +0300

    Degiskenler

---
+++ Algoritma, Programlama ve Veri Yapılarına Giriş/Degiskenler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Degiskenler
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tip
+            int sayi1;
+            double pi = 3.14;
+            char secim = 'e';
+            bool dogruMu = false;
+
+            string isim ="Ahmet";
+            Console.WriteLine("Merhaba " + isim + ".");
+
+            isim ="Mehmet";
+            Console.WriteLine("Merhaba " + isim + ".");
+
+            int sayi = 5;
+            Console.WriteLine(sayi);
+            Console.WriteLine(sayi+2);
+            Console.WriteLine(sayi*sayi);
+            Console.WriteLine(sayi-5);
+
+        }
+    } 
+}
